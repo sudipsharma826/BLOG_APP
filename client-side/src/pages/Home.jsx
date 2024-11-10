1b15d3a2ad20a1339f8675c5e90e515ea7594f51
Sudip Sharma <sudpokhara826@gmail.com>
2024-11-10 21:49:47 +0545

    Creater the nav bar compments

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const Home = () => {
   return (
+    <>
+    < NavBar />
     <h1 className='text-center mt-5'>Hello, Blog App!</h1>
+    </>
   )
 }
 
