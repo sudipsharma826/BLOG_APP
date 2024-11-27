efd70abf4425fc7e81a3049b6e45fd8aa12bb982
Sudip Sharma <sudpokhara826@gmail.com>
2024-11-27 11:51:25 +0545

    FrontEnd Part Of GoogleOuth

--- client-side/src/components/GoogleOuth.jsx
+++ client-side/src/components/GoogleOuth.jsx
@@ -1,10 +1,26 @@
 import { Button } from 'flowbite-react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
 import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'; 
+import { app } from '../fireBaseConfig';
 
 export const GoogleOuth = () => {
+    const auth = getAuth(app);
+    const handleGoogleLogin = async () => {
+        const provider = new GoogleAuthProvider();
+        provider.setCustomParameters({
+            prompt: 'select_account'
+        })
+        try{
+            const resultFromGoogle = await signInWithPopup(auth, provider);
+            
+        }catch(error){
+            console.log(error);
+            
+        }
+    }
   return (
-    <Button type='button' gradientDuoTone='pinkToOrange' outline >
+    <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleLogin}>
         <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
         Continue with Google
     </Button>
---
+++ client-side/src/fireBaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
