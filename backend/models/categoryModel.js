47e4844aca4e16dd398a81081daac37e2b3763e7
Sudip Sharma <sudpokhara826@gmail.com>
2026-05-17 09:01:22 +0545

    update the category db

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const CategorySchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true },  // Ensure category name is unique
-  imageUrl: { type: String, default: null,alias: 'catrgoryImage' },  // Store the URL of the category image
+  imageUrl: { type: String, default: null, alias: 'catrgoryImage' },
   postCount: { type: Number, default: 0 },  // Track the number of posts in this category
   createdAt: { type: Date, default: Date.now },
   
